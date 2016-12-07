1beb49c86d9b245c25de517b92e35303f0d5aa79
Konstantin Rodionov <kr@macronomica.com>
2016-12-07 12:49:58 +0300

    пример использования ssh тунеля

--- build/examples/api-ssh.js
+++ build/examples/api-ssh.js
@@ -10,16 +10,11 @@ var _2 = _interopRequireDefault(_);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const client = (0, _2.default)({ level: _.LEVEL_ALL }).api('auths', {
-  url: 'root@auths.data.backend.macronomica.com:8000',
-  ssh: {
-    privateKey: _path2.default.resolve(process.env.HOME + '/.ssh/ssh-tunel-test/id_rsa')
-  }
-});
+const ssh = {
+  privateKey: _path2.default.resolve(process.env.HOME + '/.ssh/ssh-tunel-test/id_rsa')
+};
 
-client.run().then(client => client.act({
-  api: 'auths',
-  cmd: 'verify',
-  criteria: { login: 'kniaz@example.com', password: 'kniaz' }
-})).then(client.log.info).then(() => client.end()).catch(client.log.error);
+const client = (0, _2.default)({ level: _.LEVEL_ALL }).api('media', { url: 'root@media.data.backend.macronomica.com:8000', ssh }).api('users', { url: 'root@users.data.backend.macronomica.com:8000', ssh });
+
+client.run().then(client => Promise.all([client.act({ api: 'media', cmd: 'ping' }).then(client.log.info), client.act({ api: 'users', cmd: 'ping' }).then(client.log.info)])).catch(client.log.error).then(() => client.end());
 //# sourceMappingURL=api-ssh.js.map
--- src/examples/api-ssh.js
+++ src/examples/api-ssh.js
@@ -1,20 +1,20 @@
 import path from 'path';
 import Micro, { LEVEL_ALL } from './../';
+const ssh = {
+  privateKey: path.resolve(process.env.HOME + '/.ssh/ssh-tunel-test/id_rsa')
+};
 
-const client = Micro({ level: LEVEL_ALL }).api('auths', {
-  url: 'root@auths.data.backend.macronomica.com:8000',
-  ssh: {
-    privateKey: path.resolve(process.env.HOME + '/.ssh/ssh-tunel-test/id_rsa')
-  }
-});
+const client = Micro({ level: LEVEL_ALL })
+  .api('media', { url: 'root@media.data.backend.macronomica.com:8000', ssh })
+  .api('users', { url: 'root@users.data.backend.macronomica.com:8000', ssh })
+  ;
 
 client
   .run()
-  .then(client => client.act({
-    api     : 'auths',
-    cmd     : 'verify',
-    criteria: { login: 'kniaz@example.com', password: 'kniaz' }
-  }))
-  .then(client.log.info)
+  .then(client => Promise.all([
+    client.act({ api: 'media', cmd: 'ping' }).then(client.log.info),
+    client.act({ api: 'users', cmd: 'ping' }).then(client.log.info),
+  ]))
+  .catch(client.log.error)
   .then(() => client.end())
-  .catch(client.log.error);
+;
