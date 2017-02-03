51651a0740fc6de4d3b5f9243e6c75b054835982
Konstantin Rodionov <kr@macronomica.com>
2017-02-03 12:30:19 +0300

    Переработал алгоритм подписки - чтобы пробрасывались ошибки

--- build/utils/run-subscribers.js
+++ build/utils/run-subscribers.js
@@ -10,27 +10,42 @@ var _promise2 = _interopRequireDefault(_promise);
 
 exports.default = runSubscribers;
 
-var _async = require('async');
+var _wrapped = require('error/wrapped');
 
-var _async2 = _interopRequireDefault(_async);
+var _wrapped2 = _interopRequireDefault(_wrapped);
+
+var _queue = require('queue3');
+
+var _queue2 = _interopRequireDefault(_queue);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+const InternalError = (0, _wrapped2.default)({
+  message: '{name} - {origMessage}',
+  type: 'micro.utils.run-subscribers.internal'
+});
+
 function runSubscribers(microjs, subscribers, getPromise) {
-  return new _promise2.default((resolve, reject) => _async2.default.eachSeries(subscribers, iterator, endCb(resolve, reject)));
+  return new _promise2.default((resolve, reject) => {
+    let q = new _queue2.default({ concurrency: 1 });
 
-  function iterator(subscriber, callback) {
-    let promise = getPromise(subscriber);
+    subscribers.forEach(subscriber => q.push(callback => {
+      let promise = getPromise(subscriber);
 
-    if (!promise || typeof promise.then !== 'function') {
-      promise = _promise2.default.resolve(promise);
-    }
+      if (!promise || typeof promise.then !== 'function') {
+        promise = _promise2.default.resolve(promise);
+      }
 
-    promise.then(() => callback(null)).catch(callback);
-  }
+      promise.then(() => callback(), callback);
+    }, error => {
+      console.log('exec', error, q.jobs.length);
 
-  function endCb(resolve, reject) {
-    return (err, results) => err ? reject(err) : resolve();
-  }
+      if (error) {
+        reject(InternalError(error));
+      } else if (!q.jobs.length) {
+        resolve();
+      }
+    }));
+  });
 }
 //# sourceMappingURL=run-subscribers.js.map
--- src/utils/run-subscribers.js
+++ src/utils/run-subscribers.js
@@ -1,21 +1,32 @@
-import async from 'async';
+import WrappedError from 'error/wrapped';
+import Queue3 from 'queue3';
+
+const InternalError = WrappedError({
+  message: '{name} - {origMessage}',
+  type   : 'micro.utils.run-subscribers.internal'
+});
 
 export default function runSubscribers(microjs, subscribers, getPromise) {
-  return new Promise((resolve, reject) =>
-    async.eachSeries(subscribers, iterator, endCb(resolve, reject)));
+  return new Promise((resolve, reject) => {
+    let q = new Queue3({ concurrency: 1 });
 
-  function iterator(subscriber, callback) {
-    let promise = getPromise(subscriber);
+    subscribers.forEach(subscriber => q.push(
+      callback => {
+        let promise = getPromise(subscriber);
 
-    if (!promise || typeof promise.then !== 'function') {
-      promise = Promise.resolve(promise);
-    }
+        if (!promise || typeof promise.then !== 'function') {
+          promise = Promise.resolve(promise);
+        }
 
-    promise.then(() => callback(null))
-      .catch(callback);
-  }
-
-  function endCb(resolve, reject) {
-    return (err, results) => err ? reject(err) : resolve();
-  }
+        promise.then(() => callback(), callback);
+      },
+      (error) => {
+        if (error) {
+          reject(InternalError(error));
+        } else if (!q.jobs.length){
+          resolve();
+        }
+      }
+    ));
+  });
 }
